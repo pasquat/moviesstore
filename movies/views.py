121d0275a03ce81ad050a620aa735d474a4e2ea6
Ji Hoon Kim <jkim3962@gatech.edu>
2026-01-31 22:47:04 -0500

    report

--- movies/views.py
+++ movies/views.py
@@ -17,6 +17,7 @@ def index(request):
 def show(request, id):
     movie = Movie.objects.get(id=id)
     reviews = Review.objects.filter(movie=movie)
+    reviews = reviews.exclude(reported=True)
     template_data = {}
     template_data['title'] = movie.name
     template_data['movie'] = movie
@@ -61,4 +62,11 @@ def delete_review(request, id, review_id):
     review = get_object_or_404(Review, id=review_id,
         user=request.user)
     review.delete()
+    return redirect('movies.show', id=id)
+
+@login_required
+def report_review(request, id, review_id):
+    review = get_object_or_404(Review, id=review_id)
+    review.reported = True
+    review.save()
     return redirect('movies.show', id=id)
